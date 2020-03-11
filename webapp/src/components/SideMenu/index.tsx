85d7a2514fb418a6d0bc84afd805e51174c66e0d
pdavinunes <p.davi.nunes@gmail.com>
2020-03-10 23:08:39 -0300

    Adicionando botao voltar na tela 2

--- webapp/src/components/SideMenu/index.tsx
+++ webapp/src/components/SideMenu/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useContext } from 'react';
 import cx from 'classnames';
+import {Redirect} from 'react-router-dom'
 import mainCtx from '../../contexts/mainCtx';
 import InsightLogo from '../../assets/logo-insight.png';
 import "./styles.scss";
@@ -8,26 +9,40 @@ const SideMenu: FunctionComponent = props => {
     const { menuOpen, setMenuOpen } = useContext(mainCtx);
 
     const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
+        window.location.replace("/");
     };
 
-    return (
-        <div className={cx("side-menu-container", {
-            open: menuOpen
-        })}>
-            <div onClick={toggleMenu} className="menu-icon">
-                <span className="menu-icon-line" />
-                <span className="menu-icon-line" />
-                <span className="menu-icon-line" />
+    let path = window.location.href
+
+    if(!window.location.href.endsWith('/')){
+        return (
+            <div className={cx("side-menu-container", {
+                open: menuOpen
+            })}>
+                <div onClick={toggleMenu} className="menu-icon">
+                    <span className="menu-icon-line" />
+                    <span className="menu-icon-line" />
+                    <span className="menu-icon-line" />
+                </div>
+                <div className="side-menu-body">
+                    
+                </div>
+                <div className="side-menu-footer">
+                    <img src={InsightLogo} alt='Logotipo do Insight Data Science Lab' />
+                </div>
             </div>
-            <div className="side-menu-body">
-                
+        );
+    }else{
+        return(
+            <div className={cx("side-menu-container", {
+                open: menuOpen
+            })}>
+                <div className="side-menu-footer">
+                    <img src={InsightLogo} alt='Logotipo do Insight Data Science Lab' />
+                </div>
             </div>
-            <div className="side-menu-footer">
-                <img src={InsightLogo} alt='Logotipo do Insight Data Science Lab' />
-            </div>
-        </div>
-    );
+        )
+    }
 };
 
 export default SideMenu;
